42fad07f18e721dbab281974e26fe938a24a2df3
JordanBrule <brule.jordan@gmail.com>
2019-01-25 10:07:08 +0100

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    System.out.println("Test123");
+	mangerGlace("Fraise","Vanille","Chocolat");
+  }
+public static void mangerGlace (String p1 , String p2 , String p3) {
+System.out.println("Je mange une glace parfum : " + p1 + " " + p2 + " " + p3);
+}
+
+}
